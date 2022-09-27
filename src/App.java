2044307c472d2bc2b2e8b1774bcd373aa606d96d
Vinícius Silveira <vinifs19@gmail.com>
2022-09-27 00:09:08 -0300

    Added player and enemy variables

--- src/App.java
+++ src/App.java
@@ -1,10 +1,34 @@
 import java.util.Scanner;
 import java.util.Random;
 
-
-
 public class App {
     public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+
+        boolean gameIsRunning = true;
+        
+        // player variables
+        int health = 100;
+        int attack = 30;
+        int shield = 10;
+        int dodgeChance = 15; // per cent
+        
+        // enemy variables
+
+        String[] enemies = { "wolf", "zombie", "thief", "skeleton", "witch" };
+
+        int enemyMaxHealth = 100;
+        int enemyAttackDamage = 25;
+
+        
+
+
+
+        
+
+
         
     }
 }
